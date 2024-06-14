9b7a5a546a58dec7857b97ca225029425a61d172
Ryan Cruz <cruzryan@gmail.com>
2024-06-13 17:33:17 -0700

    ChoiceButton implementation

---
+++ src/components/ChoiceButton.stories.jsx
@@ -0,0 +1,63 @@
+import ChoiceButton from './ChoiceButton';
+import { fn } from '@storybook/test';
+
+export const ChoiceButtonActionsData = {
+  onClick: fn(),
+};
+
+export default {
+  component: ChoiceButton,
+  title: 'ChoiceButton',
+  tags: ['autodocs'],
+  excludeStories: /.*Data$/,
+  args: {
+    ...ChoiceButtonActionsData
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ display: 'flex', width: '25vw' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+const choice = {
+  content: 'あそこにレストランがあります。',
+  id: '0',
+};
+
+export const Default = {
+  args: {
+    data: {
+      ...choice,
+    },
+  },
+};
+
+export const SelectedCorrect = {
+  args: {
+    data: {
+      ...choice,
+      result: 'SELECTED_CORRECT',
+    },
+  },
+};
+
+export const UnselectedCorrect = {
+  args: {
+    data: {
+      ...choice,
+      result: 'UNSELECTED_CORRECT',
+    },
+  },
+};
+
+export const Incorrect = {
+  args: {
+    data: {
+      ...choice,
+      result: 'INCORRECT',
+    },
+  },
+};
---
+++ src/components/ChoiceButton.tsx
@@ -0,0 +1,28 @@
+import { type Choice } from '@/data/exercise';
+import './ChoiceButton.css';
+
+export interface ChoiceItem extends Choice {
+  result?: 'SELECTED_CORRECT'|'UNSELECTED_CORRECT'|'INCORRECT'|undefined;
+}
+
+type ChoiceButtonProps = {
+  data: ChoiceItem;
+  onClick: (id: string) => void;
+}
+
+export default function ChoiceButton({ data, onClick }: ChoiceButtonProps) {
+  const classes = ['choicebutton'];
+  if (data.result === 'SELECTED_CORRECT') {
+    classes.push('choicebutton--selected-correct');
+  } else if (data.result === 'UNSELECTED_CORRECT') {
+    classes.push('choicebutton--unselected-correct');
+  } else if (data.result === 'INCORRECT') {
+    classes.push('choicebutton--incorrect');
+  }
+
+  return (
+    <button className={classes.join(' ')} onClick={() => onClick(data.id)} role="button">
+      {data.content}
+    </button>
+  )
+}
