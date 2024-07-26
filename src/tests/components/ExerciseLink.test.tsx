35b902740e100164d949dc954292f8ec42aa76dc
Ryan Cruz <cruzryan@gmail.com>
2024-07-25 23:15:39 -0700

    Added ExerciseLink tests, and modified pre-commit hook to run eslint on staged files via a dedicated npm command

---
+++ src/tests/components/ExerciseLink.test.tsx
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import ExerciseLink from '@/components/ExerciseLink';
+import type { ExerciseInfo } from '@/data/lesson';
+
+describe('component/ExerciseLink', () => {
+  it('renders the ExerciseLink', () => {
+    const bookId = 'xyz';
+    const lessonId = 'abc';
+    const exercise: ExerciseInfo = {
+      id: '123',
+      title: 'Lorem ipsum',
+    };
+
+    render(
+      <MemoryRouter>
+        <ExerciseLink bookId={bookId} exercise={exercise} lessonId={lessonId} />
+      </MemoryRouter>,
+    );
+
+    const link: HTMLAnchorElement = screen.getByRole('link');
+    expect(link).toBeInTheDocument();
+    const url = new URL(link.href);
+    expect(url.pathname).toBe(
+      `/${bookId}/lesson/${lessonId}/exercise/${exercise.id}`,
+    );
+  });
+});
