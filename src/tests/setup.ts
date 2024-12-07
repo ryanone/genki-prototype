af91146ee52bd5f7268cdd7cd9023519b1f54041
Ryan Cruz <cruzryan@gmail.com>
2024-12-07 14:16:02 -0800

    Upgrade happy-dom and clean up Vitest setup file

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,9 +1,6 @@
-import { afterEach, expect } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
